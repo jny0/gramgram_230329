a05185b016934a6479cc3e1e482d5e1977c7d98f
jny0 <pjy100402@naver.com>
2023-03-31 14:06:13 +0900

    회원가입 후 로그인 페이지로 이동, 환영 메시지

---
+++ src/main/java/com/ll/gramgram/standard/Util/Ut.java
@@ -0,0 +1,15 @@
+package com.ll.gramgram.standard.Util;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class Ut {
+    public static class url {
+        public static String encode(String str) {
+            try {
+                return URLEncoder.encode(str, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                return str;
+            }
+        }
+    }
+}
